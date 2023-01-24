8d0938e09c37e9671da114dcccdc4f18be4a3f20
CauãPis <71037904+Hipsflow@users.noreply.github.com>
2023-01-24 10:56:52 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { UserDetailsComponent } from './pages/user-details/user-details.component';
 import { UsersPageComponent } from './pages/users-page/users-page.component';
+import { LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
@@ -11,7 +12,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+       useHash: true,
+       preloadingStrategy: PreloadAllModules
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
